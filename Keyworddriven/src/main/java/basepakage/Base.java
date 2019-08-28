d250d49e79e8391d041b4bb24a52f1bdd572cfdd
RAJAREDDY <RAJAREDDY@RAJAREDDY-PC>
2019-08-28 16:06:59 +0530

    First Commit

---
+++ Keyworddriven/src/main/java/basepakage/Base.java
@@ -0,0 +1,55 @@
+package basepakage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Base {
+	
+	public WebDriver driver;
+	public Properties prop;
+	
+	public WebDriver init_driver(String browserName){
+		if(browserName.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+			System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+			if(prop.getProperty("headless").equals("yes")){
+				//headless mode:
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				driver = new ChromeDriver(options);
+			}else{
+				driver = new ChromeDriver();
+			}
+		} else if(browserName.equals("firefox")){
+			System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+			System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		return driver;
+	}
+	
+	public Properties init_properties(){
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream("E:\\Domain\\Keyworddriven\\src\\main\\java\\keywordcongig\\Config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
+	
+	
+	
+	
+
+}
